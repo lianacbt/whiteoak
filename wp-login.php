c388569ad7f0909bccb0b89444e1c840bfd21bdc
lianacbt <liana@cbt.io>
2025-06-10 11:37:36 -0700

    main page, robots.txt and wp-login

---
+++ wp-login.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="https://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="https://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; www.whiteoak.law, Pleasanton CA &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//www.whiteoak.law' />
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='https://www.whiteoak.law/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.22' type='text/css' media='all' />
+<link rel='stylesheet' id='digitallaw-login-style-css'  href='https://www.whiteoak.law/wp-content/themes/digitallaw/style-login.min.css?ver=4.9.22' type='text/css' media='all' />
+<style>
+		.login #login form{background-color: #f7f7f7; box-shadow: none;}
+		body.login{background-color:#ffffff;background-repeat:no-repeat;background-size:cover;background-position:center center;}
+		.login #login h1 a{
+			background: transparent url("https://www.whiteoak.law/wp-content/uploads/WhiteOakLaw-logo-new.png") no-repeat center center;width: 320px;height: 80px;
+			background-size: 100%;
+		}
+		.wp-core-ui #login .button-primary{ background: #c46729; text-shadow: none;}
+		
+		</style>	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+		<div id="login">
+		<h1><a href="https://www.whiteoak.law/" title="www.whiteoak.law, Pleasanton CA" tabindex="-1">www.whiteoak.law, Pleasanton CA</a></h1>
+	
+<form name="loginform" id="loginform" action="https://www.whiteoak.law/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="https://www.whiteoak.law/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="https://www.whiteoak.law/wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="https://www.whiteoak.law/">&larr; Back to www.whiteoak.law, Pleasanton CA</a></p>
+		
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
